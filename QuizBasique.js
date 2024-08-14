11de1362c65cb7a214df54b2ab60642fa22e8e64
eloflo41 <elo.robert41@gmail.com>
2024-08-14 07:28:48 +0200

    mise à jour pour les niveaux

--- QuizBasique.js
+++ QuizBasique.js
@@ -1,86 +1,3 @@
-const questions = [
-    {
-        question: "Le drapeau est constitué de blanc, de bleu et de rouge, les couleurs sont à l'horizontale",
-        answers: [
-            { text: 'Russie', correct: true },
-            { text: 'Portugal', correct: false },
-            { text: 'Japon', correct: false },
-        ]
-    },
-    {
-        question: "Le drapeau est constitué de bleu, de jaune et de rouge, les couleurs sont à la verticale.",
-        answers: [
-            { text: 'Allemagne', correct: false },
-            { text: 'Bulgarie', correct: false},
-            { text: 'Roumanie', correct: true},
-        ]
-    },
-    {
-        question: "Le drapeau est constitué de blanc et il contient un rond rouge.",
-        answers: [
-            { text: 'Vietnam', correct: false},
-            { text: 'Inde', correct: false},
-            { text: 'Japon', correct: true},
-        ]
-    },
-    {
-        question: "Le drapeau contient une croix jaune, deux triangles verts et deux triangles noirs.",
-        answers: [
-            { text: 'Jamaïque', correct: true},
-            { text: 'Cuba', correct: false },
-            { text: 'Suède', correct: false},
-        ]
-    },
-    {
-        question: "Le drapeau contient du bleu et du jaune, les couleurs sont à l'horizontale.",
-        answers: [
-            { text: 'Belgique', correct: false},
-            { text: 'Pologne', correct: false},
-            { text: 'Ukraine', correct: true},
-        ]
-    },
-    {
-        question: "Le drapeau contient du rouge et il a une étoile jaune au centre.",
-        answers: [
-            { text: 'Vietnam', correct: true},
-            { text: 'Iran', correct: false},
-            { text: 'Chine', correct: false},
-        ]
-    },
-    {
-        question: "Le drapeau contient du vert, du blanc et de l'orange, les couleurs sont à la verticale.",
-        answers: [
-            { text: 'Hongrie', correct: false},
-            { text: 'Irlande', correct: true},
-            { text: 'Inde', correct: false},
-        ]
-    },
-    {
-        question: "Le drapeau contient du rouge et du blanc, les couleurs sont à l'horizontale.",
-        answers: [
-            { text: 'Indonésie', correct: true},
-            { text: 'Monténégro', correct: false},
-            { text: 'Slovaquie', correct: false},
-        ]
-    },
-    {
-        question: "Le drapeau contient du noir, du jaune et du rouge, les couleurs sont à la verticale.",
-        answers: [
-            { text: 'Monaco', correct: false},
-            { text: 'Nigeria', correct: false},
-            { text: 'Belgique', correct: true},
-        ]
-    },
-    {
-        question: "Le drapeau contient du bleu clair, du noir et du blanc, les couleurs sont à l'horizontale.",
-        answers: [
-            { text: 'Suède', correct: false},
-            { text: 'Estonie', correct: true},
-            { text: 'Luxembourg', correct: false},
-        ]
-    },
-];
-
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 const nextButton = document.getElementById('next-btn');
@@ -90,16 +7,279 @@ let currentQuestionIndex = 0;
 let score = 0;
 let time = 0;
 let timer;
+let currentDifficulty = 'easy';
+let selectedQuestions = [];
+
+const questions = {
+    easy: [
+    {
+        question: "Le drapeau est constitué d'un fond blanc avec un disque rouge au centre.",
+        answers: [
+            { text: 'Japon', correct: true },
+            { text: 'Vietnam', correct: false },
+            { text: 'Tunisie', correct: false },
+        ]
+    },
+    {
+        question: "Le drapeau est consitué d'une bande rouge, une bande rouge et d'une bande rouge à la verticale avec au ventre une feuille d'érable rouge.",
+        answers: [
+            { text: 'Allemagne', correct: false },
+            { text: 'Hollande', correct: false},
+            { text: 'Canada', correct: true},
+        ]
+    },
+    {
+        question: "Le drapeau est constitué d'un fond rouge avec une étoile jaune en haut à gauche.",
+        answers: [
+            { text: 'Australie', correct: false},
+            { text: 'Mexique', correct: false},
+            { text: 'Chine', correct: true},
+        ]
+    },
+    {
+        question: "Le drapeau est consitué d'une croix jeune au centre, de 2 triangles verts et 2 triangles noir.",
+        answers: [
+            { text: 'Jamaique', correct: true},
+            { text: 'Cuba', correct: false },
+            { text: 'Belgique', correct: false},
+        ]
+    },
+    {
+        question: "Le drapeau est constitué de noir, de jaune et de rouge, les couleurs sont à la verticale.",
+        answers: [
+            { text: 'Allemagne', correct: false},
+            { text: 'Bolivie', correct: false},
+            { text: 'Belgique', correct: true},
+        ]
+    },
+    {
+        question: "Le drapeau est constiué de rouge, de jeune et de vert, les couleurs sont à l'horizontale.",
+        answers: [
+            { text: 'Bolivie', correct: true},
+            { text: 'Estonie', correct: false},
+            { text: 'Bénin', correct: false},
+        ]
+    },
+    {
+        question: "Le drapeau est constitué de vert, de blanc et de orange, les couleurs sont à la verticale.",
+        answers: [
+            { text: 'Inde', correct: false},
+            { text: 'Irlande', correct: true},
+            { text: 'Italie', correct: false},
+        ]
+    },
+    {
+        question: "Le drapeau est consitué de rouge et de blanc, les couleurs sont à l'horizontale.",
+        answers: [
+            { text: 'Indonésie', correct: true},
+            { text: 'Iran', correct: false},
+            { text: 'Laos', correct: false},
+        ]
+    },
+    {
+        question: "Le drapeau est constiué de rouge, de blanc et de vert.",
+        answers: [
+            { text: 'Colombie', correct: false},
+            { text: 'Bulgarie', correct: false},
+            { text: 'Hongrie', correct: true},
+        ]
+    },
+    {
+        question: "Le drapeau est constitué de rouge avec une croix blanche au centre.",
+        answers: [
+            { text: 'France', correct: false},
+            { text: 'Suisse', correct: true},
+            { text: 'Vietnam', correct: false},
+        ]
+    },
+],
+
+    medium: [
+    {
+        question: "Quel est la capitale de l'Espagne ?",
+        answers: [
+            { text: 'Madrid', correct: true},
+            { text: 'Lisbonne', correct: false},
+            { text: 'Berlin', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de la Suisse ?",
+        answers: [
+            { text: 'Bordeaux', correct: false},
+            { text: 'Genève', correct: false},
+            { text: 'Berne', correct: true},
+        ]
+    },
+    {
+        question: "Quel est la capitale de l'Angleterre?",
+        answers: [
+            { text: 'Liverpool', correct: false},
+            { text: 'Londres', correct: true},
+            { text: 'Manchester', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de l'Allemagne?",
+        answers: [
+            { text: 'Francfort', correct: false},
+            { text: 'Berlin', correct: true},
+            { text: 'Hambourg', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de l'Autriche ?",
+        answers: [
+            { text: 'Graz', correct: false},
+            { text: 'Vienne', correct: true},
+            { text: 'Linz', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de la Croatie ?",
+        answers: [
+            { text: 'Pozega', correct: false},
+            { text: 'Zagreb', correct: true},
+            { text: 'Vukovar', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de l'Australie ?",
+        answers: [
+            { text: 'Canberra', correct: true},
+            { text: 'Melbourne', correct: false},
+            { text: 'Mackay', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de l'Inde ?",
+        answers: [
+            { text: 'Cochin', correct: false},
+            { text: 'New Delhi', correct: true},
+            { text: 'Halda', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de la Pologne ?",
+        answers: [
+            { text: 'Varsovie', correct: true},
+            { text: 'Wroclaw', correct: false},
+            { text: 'Cracovie', correct: false},
+        ]
+    },
+    {
+        question: "Quel est la capitale de la Turquie ?",
+        answers: [
+            { text: 'Istanbul', correct: false},
+            { text: 'Ankara', correct: true},
+            { text: 'Bolu', correct: false},
+        ]
+    },
+    ],
+
+    hard: [
+    {
+        question: "Que ce passe t'il en France le premier mai ?",
+        answers: [
+            { text: 'Victoire de 1945', correct: false},
+            { text: 'Fête du travail', correct: true},
+            { text: 'Ascension', correct: false}, 
+        ]
+    },
+    {
+        question: "Quel roi de France a instauré l'édit de Nantes en 1598, mettant fin aux guerres de religion entre catholiques et protestants ?",
+        answers: [
+            { text: 'Henri IV', correct: true},
+            { text: 'Louis XVI', correct: false},
+            { text: 'Charles IX', correct: false},
+        ]
+    },
+    {
+        question: "Quel est le fleuve le plus logn de France ?",
+        answers: [
+            { text: 'La Seine', correct: false},
+            { text: 'La Loire', correct: true},
+            { text: 'Le Rhône', correct: false},
+        ]
+    },
+    {
+        question: "Quelle est la plus grande région française en termes de superficie ?",
+        answers: [
+            { text: 'La Nouvelle-Aquitaine', correct: true},
+            { text: 'Centre-Val-De-Loire', correct: false},
+            { text: 'Bretagne', correct: false},
+        ]
+    },
+    {
+        question: "Quel scientifique française est connu pour ses travaux sur la radioactivité a été la première femme à recevoir un prix Nobel ?",
+        answers: [
+            { text: 'Rosalyn Yalow', correct: false},
+            { text: 'Marie Curie', correct: true},
+            { text: 'Linda Brown Buck', correct: false},
+        ]
+    },
+    {
+        question: "Quel président français a amorcé la décolonisation de l'Algérie en 1962 ?",
+        answers: [
+            { text: 'Geoges Pompidou', correct: false},
+            { text: 'François Mitterand', correct: false},
+            { text: 'Charles de Gaulle', correct: true},
+        ]
+    },
+    {
+        question: "Quelle île française de l'océan Indien est célèbre pour son volcan actif, le Piton de la Fournaise ?",
+        answers: [
+            { text: 'La Réunion', correct: true},
+            { text: 'La Guyane', correct: false},
+            { text: 'La Martinique', correct: false},
+        ]
+    },
+    {
+        question: "Quel président français a instauré l'abolition de la peine de mort en 1981 ?",
+        answers: [
+            { text: 'François Mitterand', correct: true},
+            { text: 'Charles de Gaulle', correct: false},
+            { text: 'Georges Pompidou', correct: false},
+        ]
+    },
+    {
+        question: "Quelle femme est devenue la première ministre française en 1991 ?",
+        answers: [
+            { text: 'Elisabeth Borne', correct: false},
+            { text: 'Edith Cresson', correct: true},
+            { text: 'Martine Aubry', correct: false},
+        ]
+    },
+    {
+        question: "Qui est appelé le Roi Soleil ?",
+        answers: [
+            { text: 'Louis XIV', correct: true},
+            { text: 'Henri IV', correct: false},
+            { text: 'Charles IX', correct: false},
+        ]
+    },
+    ],
+};
 
 function startQuiz() {
+    console.log("Le script est chargé !");
+    // Réinitialiser les variables
     currentQuestionIndex = 0;
     score = 0;
     time = 0;
-    shuffleArray(questions); // Mélanger les questions au début du quiz
-    nextButton.innerHTML = 'Suivant';
-    nextButton.style.display = 'none'; // Hide the button at the start
+    nextButton.style.display = 'none';
     timerElement.innerHTML = `Temps: ${time}s`;
+
+    // Sélectionner les questions pour la difficulté actuelle
+    selectedQuestions = [...questions[currentDifficulty]];
+    
+    // Mélanger les questions
+    selectedQuestions = selectedQuestions.sort(() => 0.5 - Math.random());
+
+    // Démarrer le chronomètre
     startTimer();
+
+    // Afficher la première question
     showQuestion();
 }
 
@@ -115,10 +295,10 @@ function stopTimer() {
 }
 
 function showQuestion() {
-    resetState();
-    const currentQuestion = questions[currentQuestionIndex];
+    resetState();  // Réinitialiser l'état avant de montrer une nouvelle question
+    const currentQuestion = selectedQuestions[currentQuestionIndex];
     questionElement.innerText = currentQuestion.question;
-    
+
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
@@ -130,6 +310,7 @@ function showQuestion() {
         answerButtonsElement.appendChild(button);
     });
 }
+
 
 function resetState() {
     nextButton.style.display = 'none';
@@ -148,42 +329,59 @@ function selectAnswer(e) {
         selectedButton.classList.add('wrong');
         addPenaltyTime();
     }
-    
+
     Array.from(answerButtonsElement.children).forEach(button => {
         if (button.dataset.correct === 'true') {
             button.classList.add('correct');
         }
         button.disabled = true;
     });
-    
+
     nextButton.style.display = 'block';
-    if (currentQuestionIndex < questions.length - 1) {
+    if (currentQuestionIndex < selectedQuestions.length - 1) {
         nextButton.innerText = 'Suivant';
     } else {
-        nextButton.innerText = `Terminé - score: ${score}/${questions.length}`;
+        nextButton.innerText = `Terminé - score: ${score}/${selectedQuestions.length}`;
         stopTimer();
+        showNextLevelButton();
     }
 }
+
+function submitAnswer() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < selectedQuestions.length) {
+        showQuestion();
+    } else {
+        // Code pour terminer le quiz ou afficher les résultats
+        nextButton.innerText = `Terminé - score: ${score}/${selectedQuestions.length}`;
+        stopTimer();
+        showNextLevelButton();
+    }
+}
+
 
 function addPenaltyTime() {
     time += 10;
     timerElement.innerHTML = `Temps: ${time}s`;
 }
 
-nextButton.addEventListener('click', () => {
-    if (currentQuestionIndex < questions.length - 1) {
-        currentQuestionIndex++;
-        showQuestion();
-    } else {
-        startQuiz();
-    }
-});
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+function showNextLevelButton() {
+    const nextLevelButton = document.getElementById('next-level-btn');
+    if ((currentDifficulty === 'easy' && questions['medium']) || 
+        (currentDifficulty === 'medium' && questions['hard'])) {
+        nextLevelButton.style.display = 'inline';
     }
 }
 
+function nextLevel() {
+    if (currentDifficulty === 'easy') {
+        currentDifficulty = 'medium';
+    } else if (currentDifficulty === 'medium') {
+        currentDifficulty = 'hard';
+    }
+
+    startQuiz(); // Recommencer le quiz avec le nouveau niveau
+}
+
+// Démarrer le quiz initialement
 startQuiz();
--- QuizLgbt.js
+++ QuizLgbt.js
@@ -1,4 +1,18 @@
-const questions = [
+
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-btn');
+const timerElement = document.getElementById('timer');
+
+let currentQuestionIndex = 0;
+let score = 0;
+let time = 0;
+let timer;
+let currentDifficulty = 'easy';
+let selectedQuestions = [];
+
+const questions = {
+    "easy": [
     {
         question: "Le drapeau est constitué de rose, de violet et bleu, les couleurs sont à l'horizontale.",
         answers: [
@@ -18,7 +32,7 @@ const questions = [
     {
         question: "Le drapeau est constitué de jaune et il contient un cercle violet.",
         answers: [
-            { text: 'Lesbienne', correct: false},
+            { text: 'Lesbien', correct: false},
             { text: 'Genderqueer', correct: false},
             { text: 'Intersexe', correct: true},
         ]
@@ -79,27 +93,195 @@ const questions = [
             { text: 'Polysexuel', correct: false},
         ]
     },
-];
+    ],
 
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-const nextButton = document.getElementById('next-btn');
-const timerElement = document.getElementById('timer');
+    "medium": [
+    {
+        question: "Je suis une femme et j'aime les femme, qui suis-je ?",
+        answers: [
+            { text: 'Lesbienne', correct: true},
+            { text: 'Gay', correct: false},
+            { text: 'Pansexuel', correct: false},
+        ]
+    },
+    {
+        question: "J'aime plusieurs personne à la fois, qui suis-je ?",
+        answers: [
+            { text: 'Mulisexuel', correct: false},
+            { text: 'Omnisexuel', correct: false},
+            { text: 'Polyamoureux', correct: true},
+        ]
+    },
+    {
+        question: "Je suis de sexe femme, mais je me sent homme, je suis ?",
+        answers: [
+            { text: 'Non-binaire', correct: false},
+            { text: 'Transgenre FTM', correct: true},
+            { text: 'Transgenre MTF', correct: false},
+        ]
+    },
+    {
+        question: "Je suis un homme et j'aime une personne pour son âme, je suis ?",
+        answers: [
+            { text: 'Bisexuel', correct: false},
+            { text: 'Pansexuel', correct: true},
+            { text: 'Gay', correct: false},
+        ]
+    },
+    {
+        question: "Je ne ressent pas le besoin d'avoir un rapport sexuel, je suis ?",
+        answers: [
+            { text: 'Aronmantique', correct: false},
+            { text: 'Asexuel', correct: true},
+            { text: 'Omnisexuel', correct: false},
+        ]
+    },
+    {
+        question: "Je n'ai pas le besoin d'être amoureux, qui suis-je ?",
+        answers: [
+            { text: 'Asexuel', correct: false},
+            { text: 'Aromantique', correct: true},
+            { text: 'Omnisexuel', correct: false},
+        ]
+    },
+    {
+        question: "Je ne me considère pas comme un humains, je suis ?",
+        answers: [
+            { text: 'Xénogenre', correct: true},
+            { text: 'Transgenre FTM', correct: false},
+            { text: 'Transgenre MTF', correct: false},
+        ]
+    },
+    {
+        question: "Je suis une femme qui aime à la fois les femmes et les hommes, je suis ?",
+        answers: [
+            { text: 'Lesbienne', correct: false},
+            { text: 'Bisexuel', correct: true},
+            { text: 'Asexuel', correct: false},
+        ]
+    },
+    {
+        question: "J'ai une malformation de naissance qui fait que j'ai à la fois les attributs d'une femme et ceux d'une homme, je suis ?",
+        answers: [
+            { text: 'Intersexe', correct: true},
+            { text: 'Omnisexuel', correct: false},
+            { text: 'Transgenre', correct: false},
+        ]
+    },
+    {
+        question: "Je ne me sent ni homme, ni femme, où les deux, je suis ?",
+        answers: [
+            { text: 'Xénogenre', correct: false},
+            { text: 'Non-binaire', correct: true},
+            { text: 'Trigenre', correct: false},
+        ]
+    },
+    ],
 
-let currentQuestionIndex = 0;
-let score = 0;
-let time = 0;
-let timer;
+    "hard": [
+    {
+        question: "Où c'est passé la première marche des fiertés dans le monde ?",
+        answers: [
+            { text: 'Paris', correct: false},
+            { text: 'New York', correct: true},
+            { text: 'Oslo', correct: false}, 
+        ]
+    },
+    {
+        question: "En quel année était la première marche des fiertés ?",
+        answers: [
+            { text: '1969', correct: true},
+            { text: '1957', correct: false},
+            { text: '1981', correct: false},
+        ]
+    },
+    {
+        question: "En quel année était la première marche des fiertés en France ?",
+        answers: [
+            { text: '1986', correct: false},
+            { text: '1971', correct: true},
+            { text: '1991', correct: false},
+        ]
+    },
+    {
+        question: "A l'origine, sur le tout premier drapeau LGBT, combien y avait-il de couleurs ?",
+        answers: [
+            { text: '8', correct: true},
+            { text: '9', correct: false},
+            { text: '5', correct: false},
+        ]
+    },
+    {
+        question: "Qui a créer le tout premier drapeau LGBT ?",
+        answers: [
+            { text: 'Harvey Milk', correct: false},
+            { text: 'Gilbert Baker', correct: true},
+            { text: 'Kye Rowan', correct: false},
+        ]
+    },
+    {
+        question: "Quel est le premier pays à avoir dépénalisé l'homosexualité ?",
+        answers: [
+            { text: 'Espagne', correct: false },
+            { text: 'Russie', correct: false },
+            { text: 'France', correct: true },
+        ]
+    },
+    {
+        question: "En quelle année la France a-t-elle dépénalisé l'homosexualité ?",
+        answers: [
+            { text: '1791', correct: true },
+            { text: '1982', correct: false },
+            { text: '1804', correct: false },
+        ]
+    },
+    
+    {
+        question: "Quelles sont les lettres restantes après LGBT+ ?",
+        answers: [
+            { text: 'QIAA', correct: true},
+            { text: 'QOAP', correct: false},
+            { text: 'IAAO', correct: false},
+        ]
+    },
+    {
+        question: "Qui a fondé le mouveent LGBT ?",
+        answers: [
+            { text: 'Gilbert Baker', correct: false},
+            { text: 'Magnus Hirschfeld', correct: true},
+            { text: 'Kye Rowan', correct: false},
+        ]
+    },
+    {
+        question: "Le mouvement homosexuel à réllement fait son apparition au cours des émeutes de Stonewall à New York, en quelles année ?",
+        answers: [
+            { text: '1969', correct: true},
+            { text: '1972', correct: false},
+            { text: '1959', correct: false},
+        ]
+    },
+    ],
+};
 
 function startQuiz() {
+    console.log("Le script est chargé !");
+    // Réinitialiser les variables
     currentQuestionIndex = 0;
     score = 0;
     time = 0;
-    shuffleArray(questions); // Mélanger les questions au début du quiz
-    nextButton.innerHTML = 'Suivant';
-    nextButton.style.display = 'none'; // Hide the button at the start
+    nextButton.style.display = 'none';
     timerElement.innerHTML = `Temps: ${time}s`;
+
+    // Sélectionner les questions pour la difficulté actuelle
+    selectedQuestions = [...questions[currentDifficulty]];
+    
+    // Mélanger les questions
+    selectedQuestions = selectedQuestions.sort(() => 0.5 - Math.random());
+
+    // Démarrer le chronomètre
     startTimer();
+
+    // Afficher la première question
     showQuestion();
 }
 
@@ -115,10 +297,10 @@ function stopTimer() {
 }
 
 function showQuestion() {
-    resetState();
-    const currentQuestion = questions[currentQuestionIndex];
+    resetState();  // Réinitialiser l'état avant de montrer une nouvelle question
+    const currentQuestion = selectedQuestions[currentQuestionIndex];
     questionElement.innerText = currentQuestion.question;
-    
+
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
@@ -130,6 +312,7 @@ function showQuestion() {
         answerButtonsElement.appendChild(button);
     });
 }
+
 
 function resetState() {
     nextButton.style.display = 'none';
@@ -148,20 +331,33 @@ function selectAnswer(e) {
         selectedButton.classList.add('wrong');
         addPenaltyTime();
     }
-    
+
     Array.from(answerButtonsElement.children).forEach(button => {
         if (button.dataset.correct === 'true') {
             button.classList.add('correct');
         }
         button.disabled = true;
     });
-    
+
     nextButton.style.display = 'block';
-    if (currentQuestionIndex < questions.length - 1) {
+    if (currentQuestionIndex < selectedQuestions.length - 1) {
         nextButton.innerText = 'Suivant';
     } else {
-        nextButton.innerText = `Terminé - score: ${score}/${questions.length}`;
+        nextButton.innerText = `Terminé - score: ${score}/${selectedQuestions.length}`;
         stopTimer();
+        showNextLevelButton();
+    }
+}
+
+function submitAnswer() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < selectedQuestions.length) {
+        showQuestion();
+    } else {
+        // Code pour terminer le quiz ou afficher les résultats
+        nextButton.innerText = `Terminé - score: ${score}/${selectedQuestions.length}`;
+        stopTimer();
+        showNextLevelButton();
     }
 }
 
@@ -170,20 +366,23 @@ function addPenaltyTime() {
     timerElement.innerHTML = `Temps: ${time}s`;
 }
 
-nextButton.addEventListener('click', () => {
-    if (currentQuestionIndex < questions.length - 1) {
-        currentQuestionIndex++;
-        showQuestion();
-    } else {
-        startQuiz();
-    }
-});
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+function showNextLevelButton() {
+    const nextLevelButton = document.getElementById('next-level-btn');
+    if ((currentDifficulty === 'easy' && questions['medium']) || 
+        (currentDifficulty === 'medium' && questions['hard'])) {
+        nextLevelButton.style.display = 'inline';
     }
 }
 
+function nextLevel() {
+    if (currentDifficulty === 'easy') {
+        currentDifficulty = 'medium';
+    } else if (currentDifficulty === 'medium') {
+        currentDifficulty = 'hard';
+    }
+
+    startQuiz(); // Recommencer le quiz avec le nouveau niveau
+}
+
+// Démarrer le quiz initialement
 startQuiz();
